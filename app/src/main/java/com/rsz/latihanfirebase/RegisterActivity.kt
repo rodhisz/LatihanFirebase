735f81a1ec1321101f292d764aec4957015b63c2
rodhisz <rodhisz.45@gmail.com>
2022-04-01 07:57:31 +0700

    Initial commit

---
+++ app/src/main/java/com/rsz/latihanfirebase/RegisterActivity.kt
@@ -0,0 +1,76 @@
+package com.rsz.latihanfirebase
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.rsz.latihanfirebase.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegisterBinding
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.tvToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val email = binding.edtEmailRegister.text.toString()
+            val password = binding.edtPasswordRegister.text.toString()
+
+            //Validasi email
+            if (email.isEmpty()) {
+                binding.edtEmailRegister.error = "Email Harus Diisi"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi email tidak sesuai
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.edtEmailRegister.error = "Email Tidak Valid"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi password
+            if (password.isEmpty()) {
+                binding.edtPasswordRegister.error = "Password Harus Diisi"
+                binding.edtPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi panjang password
+            if (password.length < 6) {
+                binding.edtPasswordRegister.error = "Password Minimal 6 Karakter"
+                binding.edtPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            RegisterFirebase(email, password)
+        }
+    }
+
+    private fun RegisterFirebase(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Register Berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
